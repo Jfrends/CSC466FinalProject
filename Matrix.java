49ea9bf1030e722d1f551bc4c81243b5a86069c8
tararajagopalan <130401090+tararajagopalan@users.noreply.github.com>
2025-06-05 18:05:03 -0700

    wrote another constructor in Matrix class to process a string File Name passed in

--- Matrix.java
+++ Matrix.java
@@ -1,6 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Scanner;
 
 
 public class Matrix {
@@ -8,6 +11,47 @@ public class Matrix {
     //Constructors
     public Matrix(double[][] data) {
         this.data = data;
+    }
+
+    public Matrix(String fileName) {
+        int numCols = 0;
+        int numRows = 0;
+        File file = new File(fileName);
+        Scanner scanner = null;
+
+        //determining number of rows and columns in the data set and initializing its dimensions
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] lineElements = line.split(",");
+                numRows++;
+                numCols = lineElements.length;
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        data = new double[numRows][numCols];
+
+        for (int i = 0; i < numRows; i++)
+        {
+            //for(int j = 0; j < numCols; j++)
+            //{
+                while(scanner.hasNextLine())
+                {
+                    String line = scanner.nextLine();
+                    String[] elements = line.split(",");
+                    for(int e = 0; e < elements.length; e++)
+                    {
+                        String element = elements[e];
+                        int elementInteger = Integer.parseInt(element);
+                        double elementDouble = (double) elementInteger;
+                        data[i][e] = elementDouble;
+                    }
+                }
+            //}
+        }
     }
 
     public int numRows() {
