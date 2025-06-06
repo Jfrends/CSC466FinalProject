0a28f5e054ad7bb7ebf44017f0b8a6605d5d15b8
Josh Frendberg <57125224+Jfrends@users.noreply.github.com>
2025-06-06 01:28:53 -0700

    Update KMeansCluster.java

--- KMeansCluster.java
+++ KMeansCluster.java
@@ -1,4 +1,6 @@
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 
 public class KMeansCluster {
@@ -7,11 +9,15 @@ public class KMeansCluster {
     private int k;
     private int maxIter;
 
+    private HashMap<Point, Integer> clusterMap =  new HashMap<>();
+
+
     public KMeansCluster(List<Point> data, int k, int maxIter){
         this.points = data;
         this.clusters = new ArrayList<>();
         this.k = k;
         this.maxIter = maxIter;
+        algo();
     }
 
     public void algo(){
@@ -33,11 +39,11 @@ public class KMeansCluster {
             }
             for (Point p: points){
                 Cluster bestCluster = clusters.get(0);
-                double minDist = p.distanceTo(bestCluster.getCenroid());
+                double minDist = p.distanceTo(bestCluster.getCentroid());
 
                 for (int j = 1; j < clusters.size(); j++) {
                     Cluster cluster = clusters.get(j);
-                    double dist = p.distanceTo(cluster.getCenroid());
+                    double dist = p.distanceTo(cluster.getCentroid());
                     if (dist < minDist) {
                         minDist = dist;
                         bestCluster = cluster;
@@ -48,9 +54,9 @@ public class KMeansCluster {
 
             }
             for (Cluster cluster: clusters){
-                Point oldCentroid = cluster.getCenroid();
+                Point oldCentroid = cluster.getCentroid();
                 cluster.updateCentroid();
-                if (!Arrays.equals(oldCentroid.coordinates, cluster.getCenroid().coordinates)){
+                if (!Arrays.equals(oldCentroid.coordinates, cluster.getCentroid().coordinates)){
                     changed = true;
                 }
 
@@ -59,8 +65,33 @@ public class KMeansCluster {
                 break;
             }
         }
+        int numClusters = 0;
+        for (Cluster c : clusters){
+            numClusters++;
+            for (Point p : c.getPoints()){
+                clusterMap.put(p, numClusters);
+            }
+        }
     }
     public List<Cluster> getClusters() {
         return clusters;
+    }
+
+    public void printClusters(){
+        for (Point point : clusterMap.keySet()){
+            System.out.println(point.coordinates[0] + ", " + point.coordinates[1] + ", " + clusterMap.get(point));
+        }
+    }
+
+    public double getSilhoutteScore(){
+        ArrayList<double[]> pointsList = new ArrayList<>();
+        int[] c = new int[clusterMap.size()];
+        int i = 0;
+        for (Point point : clusterMap.keySet()){
+            c[i] = clusterMap.get(point);
+            pointsList.add(point.coordinates);
+            i++;
+        }
+        return SilhouetteScore.computeSilhouetteScore(pointsList, c);
     }
 }
